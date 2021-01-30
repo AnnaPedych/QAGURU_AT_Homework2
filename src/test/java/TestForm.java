d1a8b069005d4b8e650e2cd6a05eea8a9fa0e1bf
AnnaPedych <apedych@gmail.com>
2021-01-30 14:11:29 +0200

    opens website

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class TestForm {
+@Test
+    void selenideFieldsTest() {
+        open("https://demoqa.com/automation-practice-form");
+    }
 }
