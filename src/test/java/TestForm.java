e7025c79ddea20f0314e5e92ad13c714ac8a88e8
AnnaPedych <apedych@gmail.com>
2021-01-30 17:05:42 +0200

    datepicker

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -14,17 +14,26 @@ public class TestForm {
     }
 @Test
     void selenideFieldsTest() {
-        open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue("Anna");
-        $("#lastName").setValue("Pedych");
-        $("#userEmail").setValue("p.ann@i.ua");
+            open("https://demoqa.com/automation-practice-form");
+            $("#firstName").setValue("Anna");
+            $("#lastName").setValue("Pedych");
+            $("#userEmail").setValue("p.ann@i.ua");
+               // unable to locate element $("#gender-radio-2").$(byValue("Female")).click();
+               // element is not clickable $(".custom-control-input").selectRadio("Female");
+               // element is not clickable  $("#genterWrapper").$(".custom-control-input").selectRadio("Female");
+               // element not found $("#genterWrapper").$(byText("Female")).selectRadio("Female");
+            $("#genterWrapper").$(byText("Female")).click();
+            $("#userNumber").setValue("0123456789");
 
-           // unable to locate element $("#gender-radio-2").$(byValue("Female")).click();
-          // element is not clickable $(".custom-control-input").selectRadio("Female");
-         // element is not clickable  $("#genterWrapper").$(".custom-control-input").selectRadio("Female");
-        // element not found $("#genterWrapper").$(byText("Female")).selectRadio("Female");
-        $("#genterWrapper").$(byText("Female")).click();
-        sleep(500);
+            //вопрос - тест остановится на упавшем шаге или можно попросить игнорировать?
+
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption(4);
+            $(".react-datepicker__year-select").selectOption("1995");
+            $(".react-datepicker__day--017").click();
+            //как задать 30 и быть уверенным что выберется 30 мая, а не апреля?
+
+            sleep(500);
 
 }
 }
