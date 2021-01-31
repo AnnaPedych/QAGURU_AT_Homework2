2debb486a05397f5bc9d1d43ac677279020bd8cc
AnnaPedych <apedych@gmail.com>
2021-01-31 12:16:51 +0200

    ready without variables

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -1,10 +1,11 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import java.io.File;
 
+import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -13,6 +14,7 @@ public class TestForm {
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
+
     }
 @Test
     void selenideFieldsTest() {
@@ -48,6 +50,9 @@ public class TestForm {
             $("#uploadPicture").uploadFile(new File ("src/test/resources/03163e85-a0d5-4174-bb83-8eac88234887.png"));
             $("#currentAddress").setValue("123 Main St, App.123");
 
+    //Scroll
+            $("#submit").scrollIntoView(false);
+
             $("#state").click();
             //$(".col-md-4 col-sm-12").selectOptionContainingText("NCR"); - выбирает но не сохраняет
             $("#stateCity-wrapper").$(byText("NCR")).click();
@@ -55,8 +60,23 @@ public class TestForm {
             $("#city").click();
             $("#stateCity-wrapper").$(byText("Noida")).click();
             $("#submit").click();
+
+    //Results Verification
+            $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+            // simple and stupid: $$(".table-responsive tr").shouldHave(textsInAnyOrder("Values", "Female", "p.ann@i.ua","17 May,1995", "Anna Pedych", "0123456789","Maths", "Reading", "03163e85-a0d5-4174-bb83-8eac88234887.png", "123 Main St, App.123", "NCR Noida"));
+            $$(".table-responsive tr").filterBy(text("Student name")).shouldHave(textsInAnyOrder("Anna Pedych"));
+            $$(".table-responsive tr").filterBy(text("Student email")).shouldHave(textsInAnyOrder("p.ann@i.ua"));
+            $$(".table-responsive tr").filterBy(text("Gender")).shouldHave(textsInAnyOrder("Female"));
+            $$(".table-responsive tr").filterBy(text("Mobile")).shouldHave(textsInAnyOrder("0123456789"));
+            $$(".table-responsive tr").filterBy(text("Date of birth")).shouldHave(textsInAnyOrder("17 May,1995"));
+            $$(".table-responsive tr").filterBy(text("Subjects")).shouldHave(textsInAnyOrder("Maths, Computer Science"));
+            $$(".table-responsive tr").filterBy(text("Hobbies")).shouldHave(textsInAnyOrder("Reading"));
+            $$(".table-responsive tr").filterBy(text("Picture")).shouldHave(textsInAnyOrder("03163e85-a0d5-4174-bb83-8eac88234887.png"));
+            $$(".table-responsive tr").filterBy(text("Address")).shouldHave(textsInAnyOrder("123 Main St, App.123"));
+            $$(".table-responsive tr").filterBy(text("State and City")).shouldHave(textsInAnyOrder("NCR Noida"));
+
+    sleep(5000);
                 }
 }
 
-//Results Verification
 
