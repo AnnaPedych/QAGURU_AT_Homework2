72720b11b74464d0df41687c9feb4a0619f45f39
AnnaPedych <apedych@gmail.com>
2021-02-22 20:06:26 +0200

    added Selenoid support

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,12 +1,18 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 public class TestBase {
     @BeforeAll
     static void setup() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
         Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
     }
 }
 
