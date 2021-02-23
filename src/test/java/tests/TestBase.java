427d8f770b2cd04ad30846f8505cbb7b182c8e1a
AnnaPedych <apedych@gmail.com>
2021-02-24 00:32:00 +0200

    returned to previous version

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -10,41 +10,24 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentsHelper.*;
 
-
 public class TestBase {
     @BeforeAll
     static void setup() {
         addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-        Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.startMaximized = true;
-
-        if (System.getProperty("remote_driver") != null) {
-            // config for Java + Selenide
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
-            Configuration.browserCapabilities = capabilities;
-            Configuration.remote = System.getProperty("remote_driver");
-
-// config for Java + Selenium
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("browserName", "chrome");
-//        capabilities.setCapability("browserVersion", "87.0");
-//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-//                "enableVNC", true,
-//                "enableVideo", true
-//        ));
-//        RemoteWebDriver driver = new RemoteWebDriver(
-//                URI.create("http://selenoid:4444/wd/hub").toURL(),
-//                capabilities
-//        );
+        // config for Java + Selenide
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
+    }
+        @AfterEach
+        public void afterEach() {
+            attachScreenshot("Last screenshot");
+            attachPageSource();
+            attachAsText("Browser console logs", getConsoleLogs());
+            attachVideo();
+            closeWebDriver();
         }
     }
-
-    @AfterEach
-    public void afterEach() {
-        attachScreenshot("Last screenshot");
-        attachPageSource();
-        attachAsText("Browser console logs", getConsoleLogs());
-           }
-}
