1a65918b671fe7e02bfbcda88c66565232e63544
AnnaPedych <apedych@gmail.com>
2021-02-23 23:43:32 +0200

    added after each and helpers

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,10 +2,14 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
+
 
 public class TestBase {
     @BeforeAll
@@ -35,5 +39,15 @@ public class TestBase {
 //                capabilities
 //        );
         }
+    }
+
+    @AfterEach
+    public void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        if(System.getProperty("video_storage") != null)
+            attachVideo();
+        closeWebDriver();
     }
 }
