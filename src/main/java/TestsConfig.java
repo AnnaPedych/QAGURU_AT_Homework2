73a8596a6342ebc323ace98f2b58a63aee03aa6e
AnnaPedych <apedych@gmail.com>
2021-03-08 16:00:41 +0200

    added testsConfig file

---
+++ src/main/java/TestsConfig.java
@@ -0,0 +1,15 @@
+import org.aeonbits.owner.Config;
+
+public interface TestsConfig extends Config {
+    @Key("browserName")
+    String getBrowserName();
+
+    @Key("browserVersion")
+    String getBrowserVersion();
+
+    @Key("mode")
+    Boolean isRemote();
+
+    @Key("webDriverUrl")
+    String getWebDriverUrl();
+}
